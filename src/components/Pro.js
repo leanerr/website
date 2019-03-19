5b7819899483ff9945e7a1ed199fa74c59c24eea
Ali <real_asgari@yahoo.com>
2019-03-19 15:18:28 +0330

    Initial commit

---
+++ src/components/Pro.js
@@ -0,0 +1,44 @@
+import React,{Component} from 'react';
+import {Row , Col} from 'react-grid-system';
+import '../css/flexboxgrid.css';
+
+export default class Pro extends Component {
+    render(){
+        return(
+            <div style={{backgroundColor:'#fffee9',padding:'7%'}} >
+                <div className="comment-section">
+                    <div className="comment-section">
+                        <div className="comment-section__head">
+                            <div className="content">
+                                <h3>نقد محصولات</h3>
+                                <p>در این بخش نقد آخرین محصولات قرار میگرد تا شما بتوانید با دقت بیشتری محصولات خود را انتخاب کنید یا از خرید خود مطمئن شوید</p>
+                                <a href="#">مشاهده همه نقدها</a>
+                            </div>
+                        </div>
+                        <div className="comment-section__content">
+                            <div className="comment-box">
+                                <h2 className="comment-box__title">نقد موبایل نوت 7</h2>
+                                <hr>
+                                    <p className="comment-box__description">لورم ایپسوم یا طرح‌نما (به انگلیسی: Lorem ipsum) به متنی آزمایشی و بی‌معنی در صنعت چاپ، صفحه‌آرایی و طراحی گرافیک گفته می‌شود. طراح گرافیک از این متن به عنوان عنصری از ترکیب بندی برای پر کردن صفحه و ارایه اولیه شکل ظاهری و کلی طرح سفارش گرفته شده استفاده می نماید، تا از نظر گرافیکی نشانگر چگونگی نوعبه مشتری یا صاحب کار خود نشان دهند ... </p>
+                            </div>
+                            <div className="comment-box">
+                                <h2 className="comment-box__title">نقد موبایل نوت 7</h2>
+                                <hr>
+                                    <p className="comment-box__description">لورم ایپسوم یا طرح‌نما (به انگلیسی: Lorem ipsum) به متنی آزمایشی و بی‌معنی در صنعت چاپ، صفحه‌آرایی و طراحی گرافیک گفته می‌شود. طراح گرافیک از این متن به عنوان عنصری از ترکیب بندی برای پر کردن صفحه و ارایه اولیه شکل ظاهری و کلی طرح سفارش گرفته شده استفاده می نماید، تا از نظر گرافیکی نشانگر چگونگی نوعبه مشتری یا صاحب کار خود نشان دهند ... </p>
+                            </div>
+                            <div className="comment-box">
+                                <h2 className="comment-box__title">نقد موبایل نوت 7</h2>
+                                <hr>
+                                    <p className="comment-box__description">لورم ایپسوم یا طرح‌نما (به انگلیسی: Lorem ipsum) به متنی آزمایشی و بی‌معنی در صنعت چاپ، صفحه‌آرایی و طراحی گرافیک گفته می‌شود. طراح گرافیک از این متن به عنوان عنصری از ترکیب بندی برای پر کردن صفحه و ارایه اولیه شکل ظاهری و کلی طرح سفارش گرفته شده استفاده می نماید، تا از نظر گرافیکی نشانگر چگونگی نوعبه مشتری یا صاحب کار خود نشان دهند ... </p>
+                            </div>
+                            <div className="comment-box">
+                                <h2 className="comment-box__title">نقد موبایل نوت 7</h2>
+                                <hr>
+                                    <p className="comment-box__description">لورم ایپسوم یا طرح‌نما (به انگلیسی: Lorem ipsum) به متنی آزمایشی و بی‌معنی در صنعت چاپ، صفحه‌آرایی و طراحی گرافیک گفته می‌شود. طراح گرافیک از این متن به عنوان عنصری از ترکیب بندی برای پر کردن صفحه و ارایه اولیه شکل ظاهری و کلی طرح سفارش گرفته شده استفاده می نماید، تا از نظر گرافیکی نشانگر چگونگی نوعبه مشتری یا صاحب کار خود نشان دهند ... </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        )
+    }
+}
