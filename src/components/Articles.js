ea8c08a00096eff778299de317113d777f2d1b94
Ali <ali8asgari@gmail.com>
2019-06-04 15:26:02 +0430

    version 1

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -106,34 +106,34 @@ export default class Articles extends Component{
                             <div className='backColor2'>
 
 
-                                <img className="blogpic radius"   style={{width:'88%' , height:'200px'  , padding:'5%' , borderRadius:'15% !important'}} src={require('./../image/victor-freitas-570027-unsplash.jpg')} alt="hi"/>
+                                <img className="blogpic radius"   style={{width:'92%' , height:'210px'  , padding:'3.5%' , borderRadius:'15% !important'}} src={require('./../image/victor-freitas-570027-unsplash.jpg')} alt="hi"/>
 
-                                <div className='' style={{border:'white solid 2px'}}></div>
+                                <div className='' style={{border:'white solid 1.5px'}}></div>
 
-                                <h3 className='white '>Not Exercising May Be Worse for Your Health Than Smoking, Study Says</h3>
-                                <p className='white'>  It’s common knowledge that there are many benefits to being fit, but one large new study found that</p>
+                                <h4 className='white '>Not Exercising May Be Worse for Your Health Than Smoking, Study Says</h4>
+                                <p className='white  pSize '>  It’s common knowledge that there are many benefits to being fit, but one large new study found that</p>
                                 <Link  to={`/Article1`}><h5 className="hbtn hb-fill-middle-bg-br hbor4 hpill hpad2" >read it</h5></Link>
                             </div>
                         </Col>
                         <Col  className=" paddingBottom" sm={12} md={4} lg={4}>
                             <div className='backColor3'>
 
-                                <img className="blogpic radius" style={{width:'88%' , height:'200px' , padding:'5%' ,borderRadius:'15% !important'}} src={require('./../image/meghan-holmes-779221-unsplash.jpg')} alt="hs"/>
-                                <div className='' style={{border:'white solid 2px'}}></div>
+                                <img className="blogpic radius" style={{width:'92%' , height:'210px' , padding:'3.5%' ,borderRadius:'15% !important'}} src={require('./../image/meghan-holmes-779221-unsplash.jpg')} alt="hs"/>
+                                <div className='' style={{border:'white solid 1.5px'}}></div>
 
-                                <h3 className='white'>Fitness trackers prove helpful in monitoring cancer patients</h3>
-                                <p className='white'> Fitness trackers can be valuable tools for assessing the quality of life and daily functioning of cancer patients</p>
+                                <h4 className='white'>Fitness trackers prove helpful in monitoring cancer patients</h4>
+                                <p className='white pSize'> Fitness trackers can be valuable tools for assessing the quality of life and daily functioning of cancer patients</p>
                                 <Link  to={`/Article2`}><h5 className="hbtn hb-fill-middle-bg-br hbor4 hpill hpad2" >read it</h5></Link>
                             </div>
                         </Col>
                         <Col  className=" paddingBottom" sm={12} md={4} lg={4}>
                             <div className='backColor4'>
 
-                                <img className="blogpic radius" style={{width:'88%' , height:'200px' , padding:'5%' ,borderRadius:'15% !important'}} src={require('./../image/victor-freitas-569958-unsplash.jpg')} alt="hs"/>
-                                <div className='' style={{border:'white solid 2px'}}></div>
+                                <img className="blogpic radius" style={{width:'92%' , height:'210px' , padding:'3.5%' ,borderRadius:'% !important'}} src={require('./../image/victor-freitas-569958-unsplash.jpg')} alt="hs"/>
+                                <div className='' style={{border:'white solid 1.5px'}}></div>
 
-                                <h3 className='white'>Are you looking to build some serious muscle</h3>
-                                <p className='white'>  he Supplement Guides give you accurate information on which supplements work an you can use it</p>
+                                <h4 className='white'>Are you looking to build some serious muscle?Stop wasting your money</h4>
+                                <p className='white pSize'>  he Supplement Guides give you accurate information on which supplements work an you can use it</p>
                                 <Link  to={`/Article3`}><h5 className="hbtn hb-fill-middle-bg-br hbor4 hpill hpad2" >read it</h5></Link>
                             </div>
                         </Col>
