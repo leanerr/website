55721062040d78bc90bdc3e36585d9811a5f767e
Ali <ali8asgari@gmail.com>
2019-08-04 18:47:51 +0430

    Initial commit

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -24,9 +24,9 @@ export default class blog extends Component{
 
                     <Row className="">
                         <Col className="paddingBottom" sm={12} md={12} lg={12}>
-                            <div className='backColor'>
+                            <div className='backColor4'>
 
-                                <div className='backColor' style={{padding:'2%'}}>
+                                <div className='backColor4' style={{padding:'2%'}}>
                                     <div><h2 className='white title'>مجله ورزشی فیتکس</h2></div>
                                 </div>
                                 {/*<img className="blogpic radius"   style={{width:'92%' , height:'210px'  , padding:'3.5%' , borderRadius:'15% !important'}} src={require('./../image/victor-freitas-569958-unsplash-min (1).jpg')} alt="hi"/>*/}
@@ -34,7 +34,7 @@ export default class blog extends Component{
 
                                 <h3 className='white '>ما توی مجله ورزشیمون واستون آخرین مطالب و مقالات تخصصی ورزش و سلامت و حتی تغذیه رو  گرد آوری میکنیم</h3>
                                 <h5 className='white  pSize '>  کلیک کنید تا وارد مجله ورزشی ما بشید و بتونید از آخرین مقالات استفاده کنید</h5>
-                                <a  href='https://blog.fitexapp.ir'><h4 className="hbtn hb-fill-middle-bg-br hbor4 hpill hpad2" >مجله ورزشی فیتکس</h4></a>
+                                <a  href='https://blog.fitexapp.ir' className="hbtn hb-fill-middle-bg-br hbor4 hpill hpad2" >مجله ورزشی فیتکس</a>
                             </div>
                         </Col>
                     </Row>
