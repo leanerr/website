c5a92d468f1e27e28403b845583ecdd22212cc82
Ali <ali8asgari@gmail.com>
2020-04-03 03:38:58 +0430

    just landing

--- app/routes/web/home.js
+++ app/routes/web/home.js
@@ -6,20 +6,19 @@ const homeController = require('app/http/controllers/homeController');
 const courseController = require('app/http/controllers/courseController');
 const landController = require('app/http/controllers/landController');
 
-router.get('/logout' , (req ,res) => {
-    req.logout();
-    res.clearCookie('remember_token');
-    res.redirect('/');
-});
+// router.get('/logout' , (req ,res) => {
+//     req.logout();
+//     res.clearCookie('remember_token');
+//     res.redirect('/');
+// });
 
 // Home Routes
-router.get('/' , homeController.index);
-router.get('/post' , courseController.index);
-router.get('/posts' , courseController.posts);
+// router.get('/post' , courseController.index);
+// router.get('/posts' , courseController.posts);
 router.get('/about-us' , homeController.about);
-router.get('/post/:course' , courseController.single);
+// router.get('/post/:course' , courseController.single);
 
-router.get('/sitemap.xml' , homeController.sitemap);
-router.get('/feed/post' , homeController.feedCourses);
+// router.get('/sitemap.xml' , homeController.sitemap);
+// router.get('/feed/post' , homeController.feedCourses);
 
 module.exports = router;
--- app/routes/web/index.js
+++ app/routes/web/index.js
@@ -2,24 +2,24 @@ const express = require('express');
 const router = express.Router();
 
 // Middlewares
-const redirectIfAuthenticated = require('app/http/middleware/redirectIfAuthenticated');
-const redirectIfNotAdmin = require('app/http/middleware/redirectIfNotAdmin');
+// const redirectIfAuthenticated = require('app/http/middleware/redirectIfAuthenticated');
+// const redirectIfNotAdmin = require('app/http/middleware/redirectIfNotAdmin');
 const errorHandler = require('../../http/middleware/errorHandler');
 // Admin Router
-const adminRouter = require('app/routes/web/admin');
-router.use('/admin' , redirectIfNotAdmin.handle , adminRouter);
+// const adminRouter = require('app/routes/web/admin');
+// router.use('/admin' , redirectIfNotAdmin.handle , adminRouter);
 
 // Home Router
-const homeRouter = require('app/routes/web/home');
-router.use('/' , homeRouter);
+// const homeRouter = require('app/routes/web/home');
+// router.use('/' , homeRouter);
 
 // Landing Router
 const landRouter = require('app/routes/web/landing');
-router.use('/land' , landRouter);
+router.use('/' , landRouter);
 
 // Auth Router
-const authRouter = require('app/routes/web/auth');
-router.use('/auth' , redirectIfAuthenticated.handle ,authRouter);
+// const authRouter = require('app/routes/web/auth');
+// router.use('/auth' , redirectIfAuthenticated.handle ,authRouter);
 
 //Handle errors
 router.all('*' , errorHandler.error404);
